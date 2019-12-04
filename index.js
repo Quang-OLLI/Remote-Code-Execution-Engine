006b731281643b9531e274067d9dc66617d75086
Arav1nd2 <aravindsrinivasan2000@gmail.com>
2019-12-04 16:25:24 +0530

    C and C++ Support

--- index.js
+++ index.js
@@ -2,51 +2,95 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const DockerMananger = require("./docker-manager");
 const multer = require("multer");
-const uuid = require("uuid");
+const short = require("short-uuid");
 const fs = require("fs");
-
+const uuid = short();
 const app = express();
 
 let storage = multer.diskStorage({
      destination: function(req, file, callback) {
-          callback(null, "./project/src");
+          if (file.fieldname === "source") {
+               callback(null, "./project/src");
+          } else if (file.fieldname === "input") {
+               console.log("Im trying to store input");
+               callback(null, "./project/IO");
+          }
      },
      filename: function(req, file, callback) {
-          const ext = file.originalname.split(".")[1];
-          req.sourceFile = uuid();
-          callback(null, req.sourceFile + `.${ext}`);
+          if (file.fieldname === "source") {
+               const ext = file.originalname.split(".")[1];
+               req.sourceFile = uuid.new() + `.${ext}`;
+               callback(null, req.sourceFile);
+          } else if (file.fieldname === "input") {
+               req.inputFile = uuid.new() + ".in";
+               callback(null, req.inputFile);
+          }
      }
 });
 
-let upload = multer({ storage: storage }).single("source");
+let upload = multer({ storage: storage }).any();
 
-async function compileCode(filename) {
+function getCCmd(language, fname) {
+     if (language === "c") {
+          return [
+               "/bin/sh",
+               "-c",
+               `gcc -o /project/bin/${
+                    fname.split(".")[0]
+               } /project/src/${fname}`
+          ];
+     } else if (language === "cpp") {
+          return [
+               "/bin/sh",
+               "-c",
+               `g++ -o /project/bin/${
+                    fname.split(".")[0]
+               } /project/src/${fname}`
+          ];
+     }
+}
+
+function getECmd(language, fname, iname) {
+     if (language === "c" || language === "cpp") {
+          return [
+               "/bin/sh",
+               "-c",
+               `/project/bin/${fname.split(".")[0]} < project/IO/${iname};`
+          ];
+     }
+}
+
+async function compileCode(filename, inputFile, language) {
      let docker = new DockerMananger();
      const name = await docker.createContainer();
      console.log(`Container : ${name} created....`);
-     let resp = await docker.execute([
-          "gcc",
-          "-o",
-          `/project/bin/${filename}`,
-          `/project/src/${filename}.c`
-     ]);
-     if (resp != "") {
+     let compileCmd = getCCmd(language, filename);
+     let execCmd = getECmd(language, filename, inputFile);
+     let resp = await docker.execute(compileCmd);
+     console.log("compilation result", resp);
+     if (resp !== "") {
           docker.removeContainer();
-          fs.unlink(`./project/src/${filename}.c`, err => {
+          fs.unlink(`./project/src/${filename}`, err => {
                console.log(err);
           });
-          fs.unlink(`./project/bin/${filename}`, err => console.log(err));
+          fs.unlink(`./project/bin/${filename.split(".")[0]}`, err =>
+               console.log(err)
+          );
+          fs.unlink(`./project/IO/${inputFile}`, err => console.log(err));
+
           return {
                type: "Compilation error",
                error: resp
           };
      } else {
           // Check for infinite loops!
-          resp = await docker.execute([`/project/bin/${filename}`]);
-
+          resp = await docker.execute(execCmd);
           docker.removeContainer();
-          fs.unlink(`./project/src/${filename}.c`, err => console.log(err));
-          fs.unlink(`./project/bin/${filename}`, err => console.log(err));
+          fs.unlink(`./project/src/${filename}`, err => console.log(err));
+          fs.unlink(`./project/bin/${filename.split(".")[0]}`, err =>
+               console.log(err)
+          );
+          fs.unlink(`./project/IO/${inputFile}`, err => console.log(err));
           return {
                type: "Execution Result",
                output: resp
@@ -57,9 +101,14 @@ async function compileCode(filename) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/", upload, async (req, res, next) => {
-     const output = await compileCode(req.sourceFile);
-     res.json({ filename: req.sourceFile, output });
+app.post("/:language", upload, async (req, res, next) => {
+     const output = await compileCode(
+          req.sourceFile,
+          req.inputFile,
+          req.params.language
+     );
+     console.log("Output", output);
+     res.json({ filename: req.sourceFile, input: req.inputFile, output });
 });
 
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(4000, () => console.log("Listening on port 3000!"));
